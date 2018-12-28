ed5b0535415ca5fdab58093ff3b6c4abd2afe335
ryogaide <ide.r.aa@m.titech.ac.jp>
2018-12-28 16:07:58 +0900

    Add pptx how to restoration jpeg

--- restoration.py
+++ restoration.py
@@ -8,11 +8,13 @@ Created on Sun Dec 23 22:22:57 2018
 import pandas as pd
 import numpy as np
 import struct
+import re
+import os
 
 
-file_name = 'thumb.000'
+file_name = 'test.bin'
 
-data = np.zeros([10,16],dtype=np.uint8)
+#data = np.zeros([10,16],dtype=np.uint8)
 
 #with open(file_name, 'rb') as f:
 #   gr1[1] = f.read(1)
@@ -36,6 +38,21 @@ data = np.zeros([10,16],dtype=np.uint8)
 #        print('%02x' % (ord(d)), end=' ')
 
 
-fp = open(file_name,'rb')
-data[0,:] = np.fromfile(fp, np.uint8, 16)
-fp.close()
+#fp = open(file_name,'rb')
+#data = np.fromfile(fp, np.uint8, 16)
+#fp.close()
+
+pre_OK = np.array([0x0d, 0x0a],np.uint8)
+pre_01 = np.array([0x30, 0x31],np.uint8)
+EOF    = np.array([0xff, 0x1e],np.uint8)
+EndOfJpeg = np.array([0xff, 0xd9],np.uint8)
+
+with open(file_name, 'rb') as fp:
+    data = np.fromfile(fp, np.uint8)
+
+FileSize = data.size
+tmp_count = 0
+flag = 0
+i = 0
+j = 1
+
